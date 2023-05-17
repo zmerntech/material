3816a5cb3440ea4b449beb2dc21d8686f00f377d
Ziyauddin <zmerntech@gmail.com>
2023-05-17 18:32:51 +0530

    23 comp.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Box
+} from "@mui/material";
 
-function App() {
+import {
+  red,
+  cyan,
+  deepOrange,
+  lightGreen,
+  indigo
+} from "@mui/material/colors";
+
+import {
+  createTheme,
+  ThemeProvider
+} from "@mui/material/styles";
+
+import 'material-icons/iconfont/material-icons.css';
+
+const App = ()=>{
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: cyan['A400']
+      },
+      secondary: red,
+      warning: {
+        main: deepOrange['A400']
+      },
+      error: {
+        main: red[200]
+      },
+      success: {
+        main: lightGreen['A400']
+      }
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+
+        <Button 
+          variant="contained" 
+          size="large" 
+          sx={{
+            backgroundColor: "secondary.light"
+          }}
+        >Button</Button>
+
+        <Box sx={{width: 400, height: 200, bgcolor: "success.main"}}></Box>
+      </ThemeProvider>
+      
+      
+    </>
   );
 }
-
 export default App;
